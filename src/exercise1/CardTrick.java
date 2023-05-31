0213367522a972625f86209235188575961cdfbf
Mykyta Varnikov <mercy.dog.0104@gmail.com>
2023-05-31 00:49:07 -0400

    I DID THAT YES ITS WORKING

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -79,17 +79,17 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Mykyta, but you can call me Mike");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Stop procrastinating");
+        System.out.println("-- Have a semester with 3.5 Gpa and above");
 	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
+        System.out.println("-- Volleyball");
+        System.out.println("-- Games");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
       
